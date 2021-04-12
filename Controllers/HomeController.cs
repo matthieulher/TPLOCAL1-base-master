11f295f7bf1c63c1064eb233a2d6fe9c71e8212f
Dr. Matthieu L'HER <matthieu.lher@gmail.com>
2021-04-12 10:07:41 +0200

    Version Livrée

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text.RegularExpressions;
+using TPLOCAL1.Models;
+
+//L'énoncé du tp et le logo hn sont livrés dans le répertoire /ressources de la solution
+//--------------------------------------------------------------------------------------
+//Attention, le modèle MVC est un modèle dit de convention plutot que configuration, 
+//Le controller doit donc obligatoirement se nommer avec "Controller" à la fin!!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // méthode appelée par la routeur "naturellement"
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //renvoie vers la vue Index (voir routage dans RouteConfig.cs)
+                return View();
+            else
+            {
+                //en fonction du paramètre id, on appelle les différentes pages
+                switch (id)
+                {
+                    case "ListeAvis":
+                        //reste à faire : coder la lecture du fichier xml fourni
+                        var info = new ListeAvis();
+                        string file = @"C:\Users\Matthieu\Documents\Formation HN\ASP.NET\TPLOCAL1-base-master\TPLOCAL1-base-master\FichierXML\DataAvis.xml";
+                        return View(id, info.GetAvis(file));
+                    case "Formulaire":
+                        //reste à faire : appeler la vue Formulaire avec le modèle de données vide
+                        return View(id);
+                    default:
+                        //renvoie vers Index (voir routage dans RouteConfig.cs)
+                        return View();
+                }
+            }
+        }
+
+
+        //méthode pour envoyer les données du formulaire vers la page de validation
+        [HttpPost]
+        public ActionResult ValidationFormulaire(FormulaireModel f)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Formulaire");
+            }
+            //reste à faire : tester de si les champs du modele sont bien remplis
+            //s'ils ne sont pas bien remplis, afficher une erreur et rester sur la page formulaire
+            //sinon, appeler la page ValidationFormulaire avec les données remplies par l'utilisateur
+            return View(f);    
+        }
+    }
+}
---
+++ Models/FormulaireModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace TPLOCAL1.Models
+{
+    public class FormulaireModel
+    {
+        /// Nom de famille
+        [Display(Name = "Nom")]
+        [Required(ErrorMessage = "Nom Manquant")]
+        public string Nom { get; set; }
+
+        /// Prénom            
+        [Display(Name = "Prénom")]
+        [Required(ErrorMessage = "Prénom Manquant")]
+        public string Prenom { get; set; }
+
+        /// Sexe
+        [Display(Name = "Sexe")]
+        [Required(ErrorMessage = "Sexe Non-Renseigné")]
+        public string Sexe { get; set; }
+
+        /// Adresse
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "Adresse Manquante")]
+        public string Adresse { get; set; }
+
+        /// Code Postal
+        [Display(Name = "Code Postal")]
+        [Required(ErrorMessage = "Code Postal Manquant")]
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "Code Postal nécessite 5 chiffres")]
+        public string Cpostal { get; set; }
+
+        /// Ville
+        [Display(Name = "Ville")]
+        [Required(ErrorMessage = "Ville Manquant")]
+        public string Ville { get; set; }
+
+        /// E-mail
+        [Display(Name = "Adresse Mail")]
+        [Required(ErrorMessage = "Adresse Mail Manquant")]
+        [RegularExpression(@"^([\w]+)@([\w]+)\.([\w]+)$", ErrorMessage = "Adresse E-Mail invalide")]
+        public string Email { get; set; }
+
+        
+        /// Date de début de formation        
+        [Display(Name = "Date début formation")]
+        [Required(ErrorMessage = "Date de début de formation Manquant")]
+        [Range(typeof(DateTime), "1/1/1970", "1/1/2021", ErrorMessage = "Date antérieur au 01/01/2021")]
+        public string DateDF { get; set; }
+
+        /// Type de formation suivie
+        [Display(Name = "Formation Suivie")]
+        [Required(ErrorMessage = "Type de Formation Suivie Manquant")]
+        public string Formation { get; set; }
+
+        /// Avis Formation Cobol
+        [Display(Name = "Formation Cobol")]
+        public string Fcobol { get; set; }
+
+        /// Avis Formation C#
+        [Display(Name = "Formation C#")]
+        public string Csharp { get; set; }
+    }
+}
